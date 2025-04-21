13b742cbcf65b80e63d9c252245d93439e819311
flmarsou <marsouinflorent12@gmail.com>
2025-04-21 19:16:56 +0200

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,58 @@
+#include "config.h"
+
+static bool	initGLFW(GLFWwindow *&window)
+{
+	// GLFW: Initializes and configures
+	if (!glfwInit())
+	{
+		std::cerr << "Failed to initialize GLFW!" << std::endl;
+		return (false);
+	}
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// GLFW: Window creation
+	window = glfwCreateWindow(640, 480, "OpenGL Sandbox", NULL, NULL);
+	if (!window)
+	{
+		std::cerr << "Failed to create the window!" << std::endl;
+		glfwTerminate();
+		return (false);
+	}
+
+	glfwMakeContextCurrent(window);
+	return (true);
+}
+
+static bool	initGLAD()
+{
+	// GLAD: Loads OpenGL function pointers
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cerr << "Failed to initialize GLAD!" << std::endl;
+		glfwTerminate();
+		return (true);
+	}
+	return (true);
+}
+
+int	main()
+{
+	GLFWwindow	*window;
+
+	if (!initGLFW(window) || !initGLAD())
+		return (-1);
+
+	glClearColor(1.0f, 0.5f, 0.0f, 1.0f);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return (0);
+}
