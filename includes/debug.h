304a365a555d1511433a95ed6d7eed790bffa197
flmarsou <marsouinflorent12@gmail.com>
2025-04-21 19:52:07 +0200

    Added defines for better prints

---
+++ includes/debug.h
@@ -0,0 +1,12 @@
+#pragma once
+
+# define RED		"\e[31m"
+# define GREEN		"\e[32m"
+# define PURPLE		"\e[35m"
+# define BLUE		"\e[36m"
+# define RESET		"\e[0m"
+
+# define ERROR		RED "[x] - Error: " RESET
+# define SUCCESS	GREEN "[v] - Success: " RESET
+# define WARNING	PURPLE "[!] - Warning: " RESET
+# define USAGE		BLUE "[i] - Usage: " RESET
