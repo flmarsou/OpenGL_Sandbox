525fe501f84a69fc52f6596589f91a41a33df7d0
flmarsou <marsouinflorent12@gmail.com>
2025-04-29 15:16:23 +0200

    Added transform

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -1,12 +1,14 @@
 #version 460 core
 
-layout (location = 0) in vec2	vertexPos;
-layout (location = 1) in vec3	vertexColor;
+layout (location = 0) in vec2	aPos;
+layout (location = 1) in vec3	aColor;
 
 out vec3	vColor;
 
+uniform mat4	transform;
+
 void	main()
 {
-	gl_Position = vec4(vertexPos, 0.0, 1.0);
-	vColor = vertexColor;
+	gl_Position = transform * vec4(aPos, 0.0, 1.0);
+	vColor = aColor;
 }
