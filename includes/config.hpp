44a11918f93b8c3498299ddb1742399a70637dd1
flmarsou <marsouinflorent12@gmail.com>
2025-04-29 15:17:05 +0200

    Added constants

--- includes/config.hpp
+++ includes/config.hpp
@@ -1,21 +1,29 @@
 #pragma once
 
-# include "debug.hpp"
-
+// External Libraries
 # include <GLAD/glad.h>
 # include <GLFW/glfw3.h>
 
+// Standard Libraries
 # include <iostream>
 # include <string>
 # include <sstream>
 # include <fstream>
 
+// Standard STL Libraries
 # include <vector>
 
-# define WINDOW_WIDTH	800
-# define WINDOW_HEIGHT	800
+// Constants
+# define WINDOW_WIDTH	1000
+# define WINDOW_HEIGHT	1000
+# define WINDOW_TITLE	"OpenGL Sandbox"
 # define GRID_WIDTH		100
 # define GRID_HEIGHT	100
+# define ERROR		"\e[31m[x] - Error: \e[0m"
+# define SUCCESS	"\e[32m[v] - Success: \e[0m"
+# define WARNING	"\e[35m[!] - Warning: \e[0m"
+# define INFO		"\e[36m[i] - Info: \e[0m"
+
 
 bool	initGLFW(GLFWwindow *&window);
 bool	initGLAD();
