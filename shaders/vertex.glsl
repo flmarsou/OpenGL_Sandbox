f89d69719512fe37a222a781aa09344508241950
flmarsou <marsouinflorent12@gmail.com>
2025-04-30 14:02:51 +0200

    Removed colors from vertex

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -1,14 +1,10 @@
 #version 460 core
 
 layout (location = 0) in vec2	aPos;
-layout (location = 1) in vec3	aColor;
-
-out vec3	vColor;
 
 uniform mat4	transform;
 
 void	main()
 {
 	gl_Position = transform * vec4(aPos, 0.0, 1.0);
-	vColor = aColor;
 }
