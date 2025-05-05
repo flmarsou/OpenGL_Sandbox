0e9dc91f10c80e7d46b76c9f1647171c53a01713
flmarsou <marsouinflorent12@gmail.com>
2025-05-05 20:37:09 +0200

    Removed draw and added movement functions

--- src/block/SandBlock.cpp
+++ src/block/SandBlock.cpp
@@ -22,104 +22,49 @@ void	SandBlock::randomizeColor()
 	switch (chance)
 	{
 		case (0):
-			this->_blockColor = {1.0f, 0.905f, 0.631f};	// Very Light Yellow
+			setColor({1.0f, 0.905f, 0.631f});	// Very Light Yellow
 			break ;
 		case (1):
-			this->_blockColor = {1.0f, 0.882f, 0.533f};	// Light Yellow
+			setColor({1.0f, 0.882f, 0.533f});	// Light Yellow
 			break ;
 		case (2):
-			this->_blockColor = {1.0f, 0.859f, 0.439f};	// Yellow
+			setColor({1.0f, 0.859f, 0.439f});	// Yellow
 			break ;
 		case (3):
-			this->_blockColor = {1.0f, 0.835f, 0.341f};	// Dark Yellow
+			setColor({1.0f, 0.835f, 0.341f});	// Dark Yellow
 			break ;
 		default:
-			this->_blockColor = {1.0f, 0.784f, 0.251f};	// Very Dark Yellow
+			setColor({1.0f, 0.784f, 0.251f});	// Very Dark Yellow
 			break ;
 	}
 }
 
-void	SandBlock::draw(float x, float y, float scale, unsigned int &shader) const
-{
-	glUseProgram(shader);
-
-	// Matrix
-	glm::mat4	transform;
-	transform = glm::mat4(1.0f);
-	transform = glm::translate(transform, glm::vec3(x, y, 0.0f));		// Position
-	transform = glm::scale(transform, glm::vec3(scale, scale, 1.0f));	// Size
-
-	// Shader Transform
-	const unsigned int	pos = glGetUniformLocation(shader, "transform");
-	glUniformMatrix4fv(pos, 1, GL_FALSE, glm::value_ptr(transform));
-
-	// Shader Color
-	const unsigned int	color = glGetUniformLocation(shader, "color");
-	glUniform3fv(color, 1, glm::value_ptr(this->_blockColor));
-
-	// Draws
-	glBindVertexArray(ABlock::_VAO);
-	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-}
-
-void	SandBlock::update(Grid &grid, int x, int y)
+void	SandBlock::update(Grid &grid, const int x, const int y)
 {
 	// Security
-	if (y + 1 >= grid.getSize())
-	{
-		this->setUpdate(true);
+	if (isOnGround(grid, y))
 		return ;
-	}
 
-	// Free Falling
-	if (!grid.getBlock(x, y + 1))
-	{
-		grid.setBlock(x, y + 1, this);
-		grid.setBlock(x, y, nullptr);
-		this->setUpdate(true);
+	// Movements
+	if (fallDown(grid, x, y))
 		return ;
-	}
-	// Falling & Moving
 	else
 	{
 		const unsigned int	chance = std::rand() % 100;
+
 		if (chance < 50)
 		{
-			// Falls left
-			if (x > 0 && !grid.getBlock(x - 1, y + 1))
-			{
-				grid.setBlock(x - 1, y + 1, this);
-				grid.setBlock(x, y, nullptr);
-				this->setUpdate(true);
+			if (fallLeft(grid, x, y))
 				return ;
-			}
-			// Falls right
-			else if (x < grid.getSize() - 1 && !grid.getBlock(x + 1, y + 1))
-			{
-				grid.setBlock(x + 1, y + 1, this);
-				grid.setBlock(x, y, nullptr);
-				this->setUpdate(true);
+			else if (fallRight(grid, x, y))
 				return ;
-			}
 		}
 		else
 		{
-			// Falls right
-			if (x < grid.getSize() - 1 && !grid.getBlock(x + 1, y + 1))
-			{
-				grid.setBlock(x + 1, y + 1, this);
-				grid.setBlock(x, y, nullptr);
-				this->setUpdate(true);
+			if (fallRight(grid, x, y))
 				return ;
-			}
-			// Falls left
-			else if (x > 0 && !grid.getBlock(x - 1, y + 1))
-			{
-				grid.setBlock(x - 1, y + 1, this);
-				grid.setBlock(x, y, nullptr);
-				this->setUpdate(true);
+			else if (fallLeft(grid, x, y))
 				return ;
-			}
 		}
 	}
 }
--- src/block/SandBlock.hpp
+++ src/block/SandBlock.hpp
@@ -7,11 +7,8 @@ class	SandBlock : public ABlock
 	public:
 		SandBlock();
 
-		void	draw(float x, float y, float scale, unsigned int &shader) const override;
-		void	update(Grid &grid, int x, int y) override;
+		void	update(Grid &grid, const int x, const int y) override;
 
 	private:
-		glm::vec3	_blockColor;
-
 		void	randomizeColor();
 };
