8dbe5ccb61a449ca9e7ffceac586b746a134303c
flmarsou <marsouinflorent12@gmail.com>
2025-05-05 20:37:30 +0200

    Removed draw and added movement functions

--- src/block/WaterBlock.cpp
+++ src/block/WaterBlock.cpp
@@ -22,155 +22,86 @@ void	WaterBlock::randomizeColor()
 	switch (chance)
 	{
 		case (0):
-			this->_blockColor = {0.102f, 0.518f, 0.722f};	// Very Light Blue
+			setColor({0.102f, 0.518f, 0.722f});	// Very Light Blue
 			break ;
 		case (1):
-			this->_blockColor = {0.102f, 0.549f, 0.722f};	// Light Blue
+			setColor({0.102f, 0.549f, 0.722f});	// Light Blue
 			break ;
 		case (2):
-			this->_blockColor = {0.102f, 0.580f, 0.722f};	// Blue
+			setColor({0.102f, 0.580f, 0.722f});	// Blue
 			break ;
 		case (3):
-			this->_blockColor = {0.102f, 0.611f, 0.722f};	// Dark Blue
+			setColor({0.102f, 0.611f, 0.722f});	// Dark Blue
 			break ;
 		default:
-			this->_blockColor = {0.102f, 0.643f, 0.722f};	// Very Dark Blue
+			setColor({0.102f, 0.643f, 0.722f});	// Very Dark Blue
 			break ;
 	}
 }
 
-void	WaterBlock::draw(float x, float y, float scale, unsigned int &shader) const
-{
-	glUseProgram(shader);
-
-	// Matrix
-	glm::mat4	transform;
-	transform = glm::mat4(1.0f);
-	transform = glm::translate(transform, glm::vec3(x, y, 0.0f));		// Position
-	transform = glm::scale(transform, glm::vec3(scale, scale, 1.0f));	// Size
-
-	// Shader Transform
-	const unsigned int	pos = glGetUniformLocation(shader, "transform");
-	glUniformMatrix4fv(pos, 1, GL_FALSE, glm::value_ptr(transform));
-
-	// Shader Color
-	const unsigned int	color = glGetUniformLocation(shader, "color");
-	glUniform3fv(color, 1, glm::value_ptr(this->_blockColor));
-
-	// Draws
-	glBindVertexArray(ABlock::_VAO);
-	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-}
-
-void	WaterBlock::update(Grid &grid, int x, int y)
+void	WaterBlock::update(Grid &grid, const int x, const int y)
 {
 	const unsigned int	chance = std::rand() % 100;
 
 	if (chance == 1)
 		randomizeColor();
 
-	// Security
+	// Swap
+	if (grid.getBlock(x, y - 1) && grid.getBlock(x, y - 1)->getId() == SANDBLOCK)
+	{
+		grid.swapBlock(x, y - 1, this, x, y);
+		setUpdate(true);
+		grid.getBlock(x, y - 1)->setUpdate(true);
+		return;
+	}
+
+	// Security & Movement
 	if (y + 1 >= grid.getSize())
 	{
-		this->setUpdate(true);
+		if (chance < 50)
+		{
+			if (moveLeft(grid, x, y))
+				return ;
+			else if (moveRight(grid, x, y))
+				return ;
+		}
+		else
+		{
+			if (moveRight(grid, x, y))
+				return ;
+			else if (moveLeft(grid, x, y))
+				return ;
+		}
+		setUpdate(true);
 		return ;
 	}
 
-	// Free Falling
-	if (!grid.getBlock(x, y + 1))
-	{
-		grid.setBlock(x, y + 1, this);
-		grid.setBlock(x, y, nullptr);
-		this->setUpdate(true);
+	// Movement
+	if (fallDown(grid, x, y))
 		return ;
-	}
-	// Falling & Moving
 	else
 	{
 		if (chance < 50)
 		{
-			// Falls left
-			if (x > 0 && !grid.getBlock(x - 1, y + 1))
-			{
-				grid.setBlock(x - 1, y + 1, this);
-				grid.setBlock(x, y, nullptr);
-				this->setUpdate(true);
+			if (fallLeft(grid, x, y))
 				return ;
-			}
-			// Moves left
-			else if (x > 0 && !grid.getBlock(x - 1, y))
-			{
-				grid.setBlock(x - 1, y, this);
-				grid.setBlock(x, y, nullptr);
-				this->setUpdate(true);
+			else if (moveLeft(grid, x, y))
 				return ;
-			}
-			// Falls right
-			else if (x < grid.getSize() - 1 && !grid.getBlock(x + 1, y + 1))
-			{
-				grid.setBlock(x + 1, y + 1, this);
-				grid.setBlock(x, y, nullptr);
-				this->setUpdate(true);
+			else if (fallRight(grid, x, y))
 				return ;
-			}
-			// Moves right
-			else if (x < grid.getSize() - 1 && !grid.getBlock(x + 1, y))
-			{
-				grid.setBlock(x + 1, y, this);
-				grid.setBlock(x, y, nullptr);
-				this->setUpdate(true);
+			else if (moveRight(grid, x, y))
 				return ;
-			}
 		}
 		else
 		{
-			// Falls right
-			if (x < grid.getSize() - 1 && !grid.getBlock(x + 1, y + 1))
-			{
-				grid.setBlock(x + 1, y + 1, this);
-				grid.setBlock(x, y, nullptr);
-				this->setUpdate(true);
+			if (fallRight(grid, x, y))
 				return ;
-			}
-			// Moves right
-			else if (x < grid.getSize() - 1 && !grid.getBlock(x + 1, y))
-			{
-				grid.setBlock(x + 1, y, this);
-				grid.setBlock(x, y, nullptr);
-				this->setUpdate(true);
+			else if (moveRight(grid, x, y))
 				return ;
-			}
-			// Falls left
-			else if (x > 0 && !grid.getBlock(x - 1, y + 1))
-			{
-				grid.setBlock(x - 1, y + 1, this);
-				grid.setBlock(x, y, nullptr);
-				this->setUpdate(true);
+			else if (fallLeft(grid, x, y))
 				return ;
-			}
-			// Moves left
-			else if (x > 0 && !grid.getBlock(x - 1, y))
-			{
-				grid.setBlock(x - 1, y, this);
-				grid.setBlock(x, y, nullptr);
-				this->setUpdate(true);
+			else if (moveLeft(grid, x, y))
 				return ;
-			}
-		}
-	}
-
-	// Swapping
-	if (y > 0)
-	{
-		ABlock	*temp;
-		temp = grid.getBlock(x, y - 1);
-		if (temp && temp->getId() == SANDBLOCK)
-		{
-			grid.setBlockForce(x, y - 1, this);
-			grid.setBlockForce(x, y, temp);
-			temp->setUpdate(true);
-			this->setUpdate(true);
-			return ;
 		}
 	}
 }
--- src/block/WaterBlock.hpp
+++ src/block/WaterBlock.hpp
@@ -7,11 +7,8 @@ class	WaterBlock : public ABlock
 	public:
 		WaterBlock();
 
-		void	draw(float x, float y, float scale, unsigned int &shader) const override;
-		void	update(Grid &grid, int x, int y) override;
+		void	update(Grid &grid, const int x, const int y) override;
 
 	private:
-		glm::vec3	_blockColor;
-
 		void	randomizeColor();
 };
