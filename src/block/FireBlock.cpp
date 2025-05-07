c6aab128cb40daf7d320da572f1036a80f529768
flmarsou <marsouinflorent12@gmail.com>
2025-05-07 22:31:47 +0200

    Added fire physics

--- src/block/FireBlock.cpp
+++ src/block/FireBlock.cpp
@@ -22,31 +22,82 @@ void	FireBlock::randomizeColor()
 	switch (chance)
 	{
 		case (0):
-			setColor({1.0f, 1.0f, 0.8f});	// White
+			setColor({1.0f, 0.95f, 0.8f});	// Yellow
 			break ;
 		case (1):
-			setColor({1.0f, 0.9f, 0.0f});	// Yellow
+			setColor({1.0f, 0.85f, 0.4f});	// Orange
 			break ;
 		case (2):
-			setColor({1.0f, 0.5f, 0.0f});	// Orange
+			setColor({1.0f, 0.6f, 0.2f});	// Orange
 			break ;
 		case (3):
-			setColor({0.9f, 0.1f, 0.0f});	// Red-Orange
+			setColor({0.9f, 0.3f, 0.1f});	// Red-Orange
 			break ;
 		default:
-			setColor({0.6f, 0.0f, 0.0f});	// Red
+			setColor({0.8f, 0.1f, 0.05f});	// Red
 			break ;
 	}
 }
 
 void	FireBlock::update(Grid &grid, const int x, const int y)
 {
-	// TODO: Make fire versatile
-	const unsigned int	chance = std::rand() % 100;
-
-	if (chance < 25)
+	// Extinguish
+	if (std::rand() % 100 <= 10)
+	{
 		grid.deleteBlock(x, y);
-	(void)grid;
-	(void)x;
-	(void)y;
+		return ;
+	}
+
+	// Security & Movement
+	if (y == 0)
+	{
+		if (std::rand() % 2)
+		{
+			if (moveLeft(grid, x, y))
+				return ;
+			if (moveRight(grid, x, y))
+				return ;
+		}
+		else
+		{
+			if (moveRight(grid, x, y))
+				return ;
+			if (moveLeft(grid, x, y))
+				return ;
+		}
+	}
+
+	// Rising Movements
+	unsigned int	direction[3] = {0, 1, 2};	// Up (0), Left (1), Right (2)
+
+	// Fisher-Yates Shuffle Algorithm
+	for (int i = 2; i >= 0; i--)
+	{
+		unsigned int	chance = std::rand() % (i + 1);
+		unsigned int	temp = direction[i];
+
+		direction[i] = direction[chance];
+		direction[chance] = temp;
+	}
+
+	for (unsigned int i = 0; i < 3; i++)
+	{
+		switch (direction[i])
+		{
+			case (0):
+				if (riseUp(grid, x, y))
+					return ;
+				break ;
+
+			case (1):
+				if (riseLeft(grid, x, y))
+					return ;
+				break ;
+
+			default:
+				if (riseRight(grid, x, y))
+					return ;
+				break ;
+		}
+	}
 }
