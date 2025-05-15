b26a11f11ba21615671b09ee8528bf3b8613736b
flmarsou <marsouinflorent12@gmail.com>
2025-05-15 13:13:29 +0200

    Added FPS, VSync, and Pause Toggles

--- src/util/debug.cpp
+++ src/util/debug.cpp
@@ -1,5 +1,49 @@
 # include "config.hpp"
 
+void	togglePause()
+{
+	if (!Input::keyPauseToggle)
+	{
+		std::cout << INFO "Paused!" << std::endl;
+		Input::keyPauseToggle = true;
+	}
+	else
+	{
+		std::cout << INFO "Unpaused!" << std::endl;
+		Input::keyPauseToggle = false;
+	}
+}
+
+void	toggleVSync()
+{
+	if (!Input::keyVSyncToggle)
+	{
+		std::cout << INFO "V-Sync On!" << std::endl;
+		glfwSwapInterval(0);
+		Input::keyVSyncToggle = true;
+	}
+	else
+	{
+		std::cout << INFO "V-Sync Off!" << std::endl;
+		glfwSwapInterval(1);
+		Input::keyVSyncToggle = false;
+	}
+}
+
+void	toggleFPS()
+{
+	if (!Input::keyFPSToggle)
+	{
+		std::cout << INFO "FPS On!" << std::endl;
+		Input::keyFPSToggle = true;
+	}
+	else
+	{
+		std::cout << INFO "FPS Off!" << std::endl;
+		Input::keyFPSToggle = false;
+	}
+}
+
 void	printFPS()
 {
 	static int												frameCount = 0;
