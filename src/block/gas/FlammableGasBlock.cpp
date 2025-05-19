0e250fd93d817f13922c0fe6bf5fb4d8bc2c9b86
flmarsou <marsouinflorent12@gmail.com>
2025-05-19 16:04:55 +0200

    Fixed gas movements

--- src/block/gas/FlammableGasBlock.cpp
+++ src/block/gas/FlammableGasBlock.cpp
@@ -95,7 +95,7 @@ void	FlammableGasBlock::update(Grid &grid, const int x, const int y)
 		randomizeColor();
 
 	// Swap Above
-	if (grid.getBlock(x, y - 1) && grid.getBlock(x, y - 1)->getType() == LIQUID_TYPE)
+	if (grid.getBlock(x, y - 1) && (grid.getBlock(x, y - 1)->getType() == LIQUID_TYPE || grid.getBlock(x, y - 1)->getType() == POWDER_TYPE))
 	{
 		grid.swapBlock(x, y - 1, this, x, y);
 		setUpdate(true);
